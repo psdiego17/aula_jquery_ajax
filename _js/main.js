bda791c87fb33875c780fd517454e6d28d082f62
Diêgo Pereira Souza <psdiego17@gmail.com>
2021-03-17 15:25:47 -0300

    Configurado a implementação de consulta do cep por intérmédio do Ajax e Jquery, configuraro o CSS da página e as chamadas via JS

--- _js/main.js
+++ _js/main.js
@@ -1,1 +1,24 @@
-alert('Chamando o main.js');
+//alert('Chamando o main.js');
+
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url);
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            //alert(response.bairro);
+
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+
+            //Utilizando o Ajax
+            $("#logradouro").html("Logradouro: "+response.logradouro);
+            $("#bairro").html("Bairro: "+response.bairro);
+            $("#localidade").html("Cidade: "+response.localidade);
+            $("#uf").html("UF: "+response.uf);
+        }
+    })
+}
+
